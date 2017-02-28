52e59008d9df9eaee515509df8596025abc512b6
XGHeaven <xgheaven@gmail.com>
2017-02-28 08:55:57 +0800

    fix generator can't generate some course

--- generator/zhengfang.js
+++ generator/zhengfang.js
@@ -7,9 +7,21 @@ function Course(name, classroom, start, last, day) {
     this.day = day;
 }
 
+Course.courses = [];
+
+Course.union = function(c) {
+    for (var i=0; i<this.courses.length; i++) {
+        var course = this.courses[i];
+        if (course.name == c.name && course.start + course.last == c.start) {
+            course.last += c.last;
+            return;
+        }
+    }
+    this.courses.push(c);
+}
+
 var
-    coursePattern = /^(.*?)\n周([一二三四五六日])第([\d]+),([\d]+)节\{第([\d]+)-([\d]+)周\}\n(.*?)\n(.*?)$/,
-    courseAddPattern = /^(.*?)\n周([一二三四五六日])第([\d]+)节\{第([\d]+)-([\d]+)周\}\n(.*?)\n(.*?)$/,
+    coursePattern = /^(.*?)\n周([一二三四五六日])第((?:[\d]+)(?:,[\d]+)*)节\{第([\d]+)-([\d]+)周(?:\|(.*?))?\}\n(.*?)\n(.*?)$/,
     weeks = ['一', '二', '三', '四', '五', '六', '日'],
     url = 'http://pan.baidu.com/share/qrcode?w=500&h=500&url=',
     win = document.getElementById("iframeautoheight").contentWindow,
@@ -21,31 +33,20 @@ for (var i=0; i<tds.length; i++) {
     var
         td = tds[i],
         match = td.innerText.match(coursePattern),
-        name, classroom, start, last, day;
+        name, classroom, start, last, day, times;
 
     if (match != null) {
         name = match[1];
         classroom = match[8];
-        start = parseInt(match[3]);
-        last = parseInt(match[4]) - start + 1;
+        times = match[3].split(',');
+        start = parseInt(times[0]);
+        last = parseInt(times[times.length - 1]) - start + 1;
         day = weeks.indexOf(match[2]);
-        courses.push(new Course(name, classroom, start, last, day));
+        Course.union(new Course(name, classroom, start, last, day));
         continue;
     }
 
-    match = td.innerText.match(courseAddPattern);
-
-    if (match != null) {
-        name = match[1];
-        start = parseInt(match[3]);
-        for (var j=0; j<courses.length; j++) {
-            if (courses[j].name == name) {
-                courses[j].last = start - courses[j].start + 1;
-                break;
-            }
-        }
-    }
 }
 
-window.open(url + encodeURIComponent(JSON.stringify(courses)));
+window.open(url + encodeURIComponent(JSON.stringify(Course.courses)));
 })()
