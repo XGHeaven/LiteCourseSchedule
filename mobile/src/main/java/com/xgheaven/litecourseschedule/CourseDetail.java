402ebda2f2c6c77019d7dc5f19aa1c42cda1e47c
XGHeaven <xgheaven@gmail.com>
2017-03-16 22:03:00 +0800

    add databinding support

--- mobile/src/main/java/com/xgheaven/litecourseschedule/CourseDetail.java
+++ mobile/src/main/java/com/xgheaven/litecourseschedule/CourseDetail.java
@@ -1,6 +1,7 @@
 package com.xgheaven.litecourseschedule;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.graphics.Interpolator;
 import android.graphics.drawable.AnimatedVectorDrawable;
@@ -11,10 +12,14 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Spinner;
+
+import com.xgheaven.litecourseschedule.databinding.ActivityCourseDetailBinding;
 
 public class CourseDetail extends AppCompatActivity {
 
@@ -29,12 +34,15 @@ public class CourseDetail extends AppCompatActivity {
     private Drawable defaultBackground;
     private AnimatedVectorDrawable fabAni;
 
-    private boolean edited = false;
+    boolean mode = true;
+    ActivityCourseDetailBinding binding;
+
+    boolean edited = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course_detail);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_course_detail);
 
         courseNameColor = getResources().getColor(R.color.courseName);
 
@@ -49,32 +57,27 @@ public class CourseDetail extends AppCompatActivity {
         });
 
         CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        name = (TextInputEditText) findViewById(R.id.course_name);
-        classroom = (TextInputEditText) findViewById(R.id.course_classroom);
-        start = (TextInputEditText) findViewById(R.id.course_start);
-        last = (TextInputEditText) findViewById(R.id.course_last);
-        day = (Spinner) findViewById(R.id.course_day);
-        fab = (FloatingActionButton) findViewById(R.id.fab_course_edit);
+        name = binding.courseName;
+        classroom = binding.courseClassroom;
+        start = binding.courseStart;
+        last = binding.courseLast;
+        day = binding.courseDay;
+        fab = binding.fabCourseEdit;
 
         defaultBackground = name.getBackground();
 
         Bundle data = getIntent().getExtras();
         courseIndex = data.getInt("courseIndex");
         course = CourseList.getInstance().get(courseIndex);
-        collapsingToolbarLayout.setTitle(course.getName());
 
-        name.setText(course.getName());
-        classroom.setText(course.getClassroom());
-        start.setText(Integer.toString(course.getStart()));
-        last.setText(Integer.toString(course.getLast()));
-        day.setSelection(course.getDay());
+        binding.setCourse(course);
+        binding.courseDay.setSelection(course.getDay());
 
         fabAni = (AnimatedVectorDrawable) getDrawable(R.drawable.ic_edit_animate);
         fab.setImageDrawable(fabAni);
 
         disable();
         fab.setOnClickListener(new View.OnClickListener() {
-            boolean mode = true;
             @Override
             public void onClick(View v) {
                 if (mode) {
@@ -89,6 +92,20 @@ public class CourseDetail extends AppCompatActivity {
                     }
                 }
                 mode = !mode;
+            }
+        });
+
+        binding.courseName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String string = s.toString();
+                binding.collapsingToolbar.setTitle(string);
             }
         });
     }
@@ -130,22 +147,6 @@ public class CourseDetail extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
     private void save() {
         course.setName(name.getText().toString());
         course.setClassroom(classroom.getText().toString());
@@ -155,10 +156,8 @@ public class CourseDetail extends AppCompatActivity {
 
         edited = true;
 
-
         Intent intent = new Intent();
         intent.putExtra("editedIndex", edited ? courseIndex : -1);
         setResult(100, intent);
     }
 }
-
